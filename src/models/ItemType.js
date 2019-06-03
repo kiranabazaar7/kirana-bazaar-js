c148f6444fe0d53c185052de8349d0c99478c778
Sai Kalyan Moguloju <sai.kalyanmoguloju@cerner.com>
2019-06-03 13:17:40 -0500

    ISSUE-1:Create template project and basic models

---
+++ src/models/ItemType.js
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright &copy 2019 Kirana Bazaar
+ *
+ * @author Sai Kalyan Moguloju
+ */
+const ItemType = {
+  SINGLE: 'SINGLE',
+  DOZEN: 'DOZEN',
+  KG: 'KG',
+};
+
+export default ItemType;
